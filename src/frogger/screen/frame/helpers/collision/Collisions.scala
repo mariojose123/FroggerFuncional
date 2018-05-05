de307bd2c26df3f47134593190d00c445f82bb3b
Unknown <lauracorssac@hotmail.com>
2018-05-05 17:53:55 -0300

    car movements

--- src/frogger/screen/frame/helpers/collision/Collisions.scala
+++ src/frogger/screen/frame/helpers/collision/Collisions.scala
@@ -3,14 +3,20 @@ package frogger.screen.frame.helpers.collision
 import frogger.screen.frame.elements.frog.Frog
 import frogger.screen.frame.elements.player.PlayerStatus
 import javafx.scene.Node
+import javafx.stage.Stage
 
 import scala.collection.JavaConversions.asScalaBuffer
 
 object Collisions {
 
 
-  def onUpdate(carList: java.util.ArrayList[Node], frog: Frog):  PlayerStatus.Value = {
-    for (car <- carList) car.setTranslateX(car.getTranslateX + Math.random() * 10)
+  def onUpdate(carList: java.util.ArrayList[Node], frog: Frog, stage: Stage):  PlayerStatus.Value = {
+    for (car <- carList) {
+      if (car.getTranslateX > stage.getWidth){
+        car.setTranslateX(0)
+      }
+      car.setTranslateX(car.getTranslateX + Math.random() * 10)
+    }
     return checkState(carList, frog)
   }
 
