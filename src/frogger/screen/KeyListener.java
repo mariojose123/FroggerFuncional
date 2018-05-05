3c983d11b4906b92bf0a1a23d344f0a675d13bab
Lucas Hagen <lucas.hagen@inf.ufrgs.br>
2018-05-04 23:58:08 -0300

    Added function for event handling.

---
+++ src/frogger/screen/KeyListener.java
@@ -0,0 +1,12 @@
+package frogger.screen;
+
+import javafx.event.EventHandler;
+
+/**
+ * @author Lucas Hagen.
+ */
+public interface KeyListener {
+
+    void onKeyPressed(final EventHandler eventHandler);
+
+}
--- src/frogger/screen/Main.java
+++ src/frogger/screen/Main.java
@@ -2,6 +2,8 @@ package frogger.screen;
 
 import frogger.screen.frame.GameFrame;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,23 +13,23 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     private GameFrame game;
+    Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainscreen.fxml"));
-        primaryStage.setTitle("Frogger - MLP");
-        primaryStage.setScene(new Scene(root, 1260, 720));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-
-        //this.game = new GameFrame();
-
-        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> System.out.println("Check: " + event.getCharacter()));
-
+        stage.setTitle("Frogger - MLP");
+        stage.setScene(new Scene(root, 1260, 720));
+        stage.setResizable(false);
+        stage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void addEventHandler(EventHandler<? super KeyEvent> handler) {
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, handler);
     }
 }
