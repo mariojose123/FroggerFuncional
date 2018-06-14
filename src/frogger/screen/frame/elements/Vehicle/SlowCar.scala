0d14bc8eac459e85f68666b2e53f7fb06dd4df84
Catarina <caca_rosenau@hotmail.com>
2018-06-13 22:23:17 -0300

    removed unused packages and classes

--- src/frogger/screen/frame/elements/Vehicle/SlowCar.scala
+++
@@ -1,7 +0,0 @@
-package frogger.screen.frame.elements.Vehicle
-
-//class SlowCar extends Car(new ImageView("star.png")) {
-//
-//  setTexture(getClass.getResource("star.png").toString, 49, 24)
-//
-//}
--- src/frogger/screen/frame/elements/Vehicle/SportCar.scala
+++
@@ -1,7 +0,0 @@
-package frogger.screen.frame.elements.Vehicle
-//
-//class SportCar extends Car{
-//
-//  setTexture(getClass.getResource("car.png").toString, 49, 24)
-//
-//}
